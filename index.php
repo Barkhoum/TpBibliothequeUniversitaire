0d833c216c9fcdd36f5eaec36f6363372d36e26f
Barkhoum <90609076+Barkhoum@users.noreply.github.com>
2021-12-01 21:52:22 +0100

    ajout code html

--- index.php
+++ index.php
@@ -12,47 +12,121 @@
 </head>
 
 <body>
-    <header class="conteneur">
+    <header class="container-fluid">
         <div class="row">
-            
-                <div class="col-8">
-                    <img src="./logo.png">
-                </div>
-                <div class="col-4">
-                    <ul class="nav justify-content-end">
-                        <li class="nav-item">Espace membres
-                            <a class="nav-link " href="#">Connexion</a>
-                        </li>
-                    </ul>
-                </div>
-            git 
+            <div class="col-8">
+                <img src="./logov2.png">
+            </div>
+            <div class="col-4">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">Espace membres
+                        <a class="nav-link " href="#">Connexion</a>
+                    </li>
+                </ul>
+            </div>
         </div>
         <nav class="bg-danger">
             <ul class="nav justify-content-end">
+
                 <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
+                    <a class="nav-link active" href="#">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Infos pratique</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Ouvrages</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="#">Editeurs</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Auteurs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+
             </ul>
         </nav>
     </header>
-    <main>
-        <section>
-            <section></section>
-            <aside>
-
+    <main class="container-fluid m-2">
+        <section class="row form_recherche">
+            <div class="col-3 ">
+                <img class="m-5" src="image23.jpg" alt="photo d'une femme">
+            </div>
+            <div class="col-5">
+                <main class="container-fluid m-5">
+                    <h2>Recherchez votre ouvrage </h2>
+                    <form method="get" action="" class="row  ">
+                        <div class="mb-3 col-10">
+                            <div class="mb-3 col">
+                                <label for="recherche_livre" class="form-label "></label>
+                                <input type="text" class="form-control" name="recherche_livre" id="recherche_livre" placeholder="Chercher un livre">
+                            </div>
+                            <div class="mb-3 col">
+                                <label for="ouvrages" class="form-label"></label>
+                                <input type="text" class="form-control" name="ouvrages" id="ouvrages" placeholder="Recherche par ouvrages">
+                            </div>
+                            <div class="mb-3 col">
+                                <label for="auteurs" class="form-label"></label>
+                                <input type="text" class="form-control" name="auteurs" id="auteurs" placeholder="rechercher par auteurs">
+                            </div>
+                            <div class="mb-3 col">
+                                <label for="editeurs" class="form-label"></label>
+                                <input type="text" class="form-control" name="editeurs" id="editeurs" placeholder="rechercher par editeurs">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Rechercher</button>
+                        </div>
+                    </form>
+            </div>
+            <aside class="col-3">
+                <div class="mt-5 ">
+                    <h1>Inscrivez-vous !</h1>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">Favoris</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Catégories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Collections</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
             </aside>
         </section>
-
     </main>
+    <footer class="container-fluid p-5 m-2">
+        <div class="row">
+            <ul class="col-4">
+                <li>A propos</li>
+                <li>Reglement</li>
+                <li>Inscription</li>
+                <li>Engagement</li>
+                <li>La bibliothèque</li>
+            </ul>
+            <ul class="col-4">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <address class="col-4">
+                <a href="mailto:info@bu-juvisy.com">info@bu-juvisy.com</a><br>
+                <a href="tel:+330160606060">Tel.01 60 60 60 60</a>
+            </address>
+        </div>
+
+        <div class="text-center bg-black text-white p-3">
+            © 2021 Bibliothèque universitaire de Juvisy sur Orge
+        </div>
+    </footer>
     <script src='app.js'></script>
 </body>
 
