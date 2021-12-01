1fa6be19d600d5fd8f993f5dbccb2d72df180209
Barkhoum GUESMIA <guesmia.barkhoum@gmal.com>
2021-12-01 14:32:15 +0100

    ajout du code d'integration

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <title>Parcours Média</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="conteneur">
+        <div class="row">
+            
+                <div class="col-8">
+                    <img src="./logo.png">
+                </div>
+                <div class="col-4">
+                    <ul class="nav justify-content-end">
+                        <li class="nav-item">Espace membres
+                            <a class="nav-link " href="#">Connexion</a>
+                        </li>
+                    </ul>
+                </div>
+            git 
+        </div>
+        <nav class="bg-danger">
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <section></section>
+            <aside>
+
+            </aside>
+        </section>
+
+    </main>
+    <script src='app.js'></script>
+</body>
+
+</html>
